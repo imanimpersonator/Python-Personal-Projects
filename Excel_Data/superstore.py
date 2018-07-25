5ec457173e2f777e749778ea4ce6ade957cd0999
Mitchell S <32205080+mysstone@users.noreply.github.com>
2018-07-25 14:56:28 -0600

    Update superstore.py

--- Excel_Data/superstore.py
+++ Excel_Data/superstore.py
@@ -6,36 +6,46 @@ import time as time
 
 
 start = time.time()
+
+# Reads excel file in external_files. Refer to file to see columns
+
+
 df = pd.read_excel("external_files/Superstore.xls")
 
-total = sum(df["Order ID"].apply(lambda x: x.split("-")[0]) == "CA")
+df.head()
 
-total
+# Counts how many Order ID's start with CA
+totca = sum(df["Order ID"].apply(lambda x: x.split("-")[0]) == "CA")
 
-tot = sum(df["Order ID"].apply(lambda x: x.split("-")[0]) == "US")
+# Same as above, but with "US" Prefix
+totus = sum(df["Order ID"].apply(lambda x: x.split("-")[0]) == "US")
 
+# Finds all customers with First name that starts with "S"
 snames = df[df["Customer Name"].apply(lambda x: x.startswith("S"))]
 
-pt = pd.pivot_table(df, index = "Customer Name")
 
+# simple pivot table
+pt = pd.pivot_table(df, index = "Customer Name")
 
 pt2 = pd.pivot_table(snames, index = "Customer Name")
 
 # counts data entries of customers who's first name starts with 'S'
 i = snames["Customer Name"].value_counts()
 
-
+# tried this to split names by surname
 # surname = df[df["Customer Name"].apply(lambda x: x.split(" "))[1]]
 
+
+
+# But this ended up working better:
 df['FirstName'] = df['Customer Name'].str.split().str[0]
 df["LastName"] = df["Customer Name"].str.split().str[1]
 
+# Searches in Segment column for categories that are either "Consumer" or "Corporate"
+f = df.query('Segment == ["Consumer", "Corporate"]')
 
 
-#f = df.query('Segment == ["Consumer", "Corporate"]')
-
-
-
+# Shows counts of most common first and last names:
 fname = df["FirstName"].value_counts()
 lname = df["LastName"].value_counts()
 
@@ -53,13 +63,12 @@ df_final = df.append(df_sum, ignore_index=True)
 
 # sns.countplot(data = df, x = df["FirstName"])
 
-#writer = pd.ExcelWriter("NewSheet.xlsx", engine = "xlsxwriter")
-#
-#df.to_excel(writer, sheet_name = "superstore_data")
-#
-#
-#
-#writer.save()
+# Writes DataFrame above into new excel file, and into New Sheet
+"""writer = pd.ExcelWriter("NewSheet.xlsx", engine = "xlsxwriter")
+
+df.to_excel(writer, sheet_name = "superstore_data")
+
+writer.save()"""
 
 end = time.time()
 
